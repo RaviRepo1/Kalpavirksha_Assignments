2aabb1fc7fa27ed04a6a7a103b47b11b85b0dc67
gekkyravi <58577612+RaviRepo1@users.noreply.github.com>
2025-10-07 01:01:23 +0530

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,211 +1,165 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <limits.h>
+#include "stack.c"
+#include <ctype.h>
 
-struct stack
+int getPriority(char operator)
 {
-    long *st;
-    int size;
-    int top;
-};
+    return (operator == '+' || operator == '-') ? 1 : 2;
+}
 
-void push(struct stack *stk, long ch)
+bool isOperand(char character)
 {
-    if (stk->top == stk->size - 1)
-    {
-        printf("memory failure please try again.");
-        return;
-    }
+    return character >= '0' && character <= '9';
+}
+
+bool isOperator(char character)
+{
+    return character == '+' || character == '-' || character == '*' || character == '/';
+}
+
+
+char* convertToPostfix(char infix[]) {
+    static char postfix[2 * MAX_SIZE];
+    Stack ops = {.top = -1, .capacity = MAX_SIZE};
+    int i = 0, j = 0;
+
+    while (infix[i]) {
+        if (isdigit(infix[i])) {
+
+            while (isdigit(infix[i])) postfix[j++] = infix[i++];
+            postfix[j++] = ' ';
+        }
         
-    stk->top++;
-    stk->st[stk->top] = ch;
-}
-
-void pop(struct stack *stk)
-{
-    if (stk->top == -1)
-        return;
-    stk->top--;
-}
-
-long top(struct stack *stk)
-{
-    if (stk->top == -1)
-        return '\0';
-    return stk->st[stk->top];
-}
-
-int isempty(struct stack *stk)
-{
-    return stk->top == -1;
-}
-
-
-int priority(char c)
-{
-    return (c == '+' || c == '-') ? 1 : 2;
-}
-
-
-int isoperand(char c)
-{
-    return c >= '0' && c <= '9';
-}
-
-int isoperator(char c)
-{
-    return c == '+' || c == '-' || c == '*' || c == '/';
-}
-
-char *postfixconversion(char str2[])
-{
-    int l = strlen(str2);
-    char *str = (char *)malloc(sizeof(char) * (2 * l + 1)); 
-    struct stack s;
-    s.top = -1;
-    s.size = l;
-    s.st = (long *)malloc(sizeof(long) * l);
-    int k = 0;
-
-    for (int i = 0; i < l; i++)
-    {
-        if (isoperand(str2[i]))
-        {
-            
-            while (i < l && isoperand(str2[i]))
-            {
-                str[k++] = str2[i++];
+         else if (infix[i] == ' ') {
+            i++;
+        } 
+        
+        else if (isOperator(infix[i])) {
+           
+            while (!isEmpty(&ops) && getPriority(peek(&ops)) >= getPriority(infix[i])) {
+                postfix[j++] = pop(&ops);
+                postfix[j++] = ' ';
             }
-            str[k++] = ' '; // Add a space to separate numbers
-            i--;            // Adjust loop index
-        }
-        else if (str2[i] == ' ')
-        {
-            continue; // Ignore spaces
-        }
-        else
-        {
-            while (!isempty(&s) && priority(str2[i]) <= priority((char)top(&s)))
-            {
-                str[k++] = (char)top(&s);
-                str[k++] = ' '; 
-                pop(&s);
-            }
-            push(&s, str2[i]);
+            push(&ops, infix[i++]);
+        } 
+        
+        else {
+            i++; 
         }
     }
 
-    while (!isempty(&s))
-    {
-        str[k++] = (char)top(&s);
-        str[k++] = ' ';
-        pop(&s);
+    while (!isEmpty(&ops)) {
+        postfix[j++] = pop(&ops);
+        postfix[j++] = ' ';
     }
 
-    str[k] = '\0'; 
-    free(s.st);
-    return str;
+    postfix[j] = '\0';
+    return postfix;
 }
 
-long evaluateans(char str2[])
-{
-    char *str = postfixconversion(str2); 
-    int l = strlen(str);
-    struct stack s;
-    s.size = l;
-    s.top = -1;
-    s.st = (long *)malloc(sizeof(long) * l);
-    int a, b;
-    int num = 0;
-    int isReadingNum = 0; 
 
-    for (int i = 0; i < l; i++)
+long evaluatePostfix(char infix[])
+{
+    char *postfix = convertToPostfix(infix);
+    Stack valueStack = {.top = -1, .capacity = MAX_SIZE};
+
+    int length = strlen(postfix);
+    int number = 0;
+    bool readingNumber = false;
+
+    for (int i = 0; i < length; i++)
     {
-        if (isoperand(str[i]))
+        if (isOperand(postfix[i]))
         {
-            num = num * 10 + (str[i] - '0');
-            isReadingNum = 1;
+            number = number * 10 + (postfix[i] - '0');
+            readingNumber = true;
         }
-        else if (str[i] == ' ')
+        else if (postfix[i] == ' ')
         {
-            if (isReadingNum)
+            if (readingNumber)
             {
-                push(&s, num); 
-                num = 0;      
-                isReadingNum = 0;
+                push(&valueStack, number);
+                number = 0;
+                readingNumber = false;
             }
         }
         else
         {
-            b = top(&s);
-            pop(&s);
-            a = top(&s);
-            pop(&s);
-            switch (str[i])
+            long operand2 = peek(&valueStack);
+            pop(&valueStack);
+            long operand1 = peek(&valueStack);
+            pop(&valueStack);
+
+            switch (postfix[i])
             {
             case '+':
-                push(&s, a + b);
+                push(&valueStack, operand1 + operand2);
                 break;
             case '-':
-                push(&s, a - b);
+                push(&valueStack, operand1 - operand2);
                 break;
             case '*':
-                push(&s, a * b);
+                push(&valueStack, operand1 * operand2);
                 break;
             case '/':
-                if (b == 0)
+                if (operand2 == 0)
                 {
                     printf("Error: Division by zero.\n");
-                    free(s.st);
-                    free(str);
-                    return __INT32_MAX__;
+                    return INT_MAX;
                 }
-                push(&s, a / b);
+                push(&valueStack, operand1 / operand2);
                 break;
             }
         }
     }
 
-    long ans = top(&s);
-    free(s.st);
-    free(str);
-    return ans;
+    return peek(&valueStack);
 }
 
 int main()
 {
-    char str[100], str2[100];
-    printf("Enter the Expression:");
-    scanf("%[^\n]s", str);
+    char inputExpression[100];
+    char clearExpression[100];
 
-    int l = strlen(str), k = 0;
+    printf("Enter the Expression: ");
+    scanf("%[^\n]s", inputExpression);
 
-    for (int i = 0; i < l; i++)
+    int inputLength = strlen(inputExpression);
+    int cleanIndex = 0;
+
+    for (int i = 0; i < inputLength; i++)
     {
-        if (!isoperand(str[i]) && !isoperator(str[i]) && str[i] != ' ')
+        char currentChar = inputExpression[i];
+
+        if (!isOperand(currentChar) && !isOperator(currentChar) && currentChar != ' ')
         {
-            printf("Error: Invalid expression.");
-            exit(0);
+            printf("Error: Invalid character in expression.\n");
+            return 1;
         }
-        else if (isoperator(str[i]))
+
+        if (isOperator(currentChar))
         {
-            if ((i + 1 < l && isoperator(str[i + 1])) ||
-                (i + 2 < l && str[i + 1] == ' ' && isoperator(str[i + 2])))
+            if ((i + 1 < inputLength && isOperator(inputExpression[i + 1])) ||
+                (i + 2 < inputLength && inputExpression[i + 1] == ' ' && isOperator(inputExpression[i + 2])))
             {
-                printf("Error: Invalid expression. ");
-                exit(0);
+                printf("Error: Invalid expression.\n");
+                return 1;
             }
         }
-        {
-            str2[k++]=str[i];
-        }
-        
+
+        clearExpression[cleanIndex++] = currentChar;
     }
 
-    str2[k] = '\0';
+    clearExpression[cleanIndex] = '\0';
 
-    long ans = evaluateans(str2);
-    if (ans != __INT32_MAX__)
-        printf("Result: %ld\n", ans);
+    long result = evaluatePostfix(clearExpression);
+    if (result != INT_MAX)
+    {
+        printf("Result: %ld\n", result);
+    }
+
     return 0;
 }
